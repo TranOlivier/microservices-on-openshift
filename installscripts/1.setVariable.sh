095f54f8add4fa1c438a81878de95ec7ba27fbc7
Olivier <olivier.tran.al@gmail.com>
2018-04-23 16:24:53 +0200

    commit de test 2

--- installscripts/1.setVariable.sh
+++ installscripts/1.setVariable.sh
@@ -3,8 +3,8 @@ export OSE_DOMAIN=172.20.12.97:8443
 export OSE_CLIENT_PROJECT=msservices
 export OSE_SERVICES_PROJECT=msservices
 export OSE_INFRA_PROJECT=msservices
-export FROM_GMAIL=<<from email for your email service>>
-export FROM_GMAIL_PASSWORD=<<password for the from email>>
+export FROM_GMAIL=olivier.tran.al@gmail.com
+export FROM_GMAIL_PASSWORD=bibi
 //For these params refer https://dev.twitter.com/oauth/overview/application-owner-access-tokens
 export TWITTER_CONSUMER_KEY=<<Consumer Key (API Key)>>
 export TWITTER_CONSUMER_SECRET=<<Consumer Secret (API Secret)>>
